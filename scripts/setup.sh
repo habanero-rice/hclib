fd77fbf0fa37991cd07739628a30a9740d22ab6d
Vivek Kumar <vivekk@rice.edu>
2015-09-09 23:24:02 -0500

    fix libxml2 break

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,9 @@
 
 #export TBB_MALLOC=/Users/vivek/rice_work/projects/tbb/mac
 #export HCPP_FLAGS="--enable-production"
-export LIBXML2=/usr/local/Cellar/libxml2/2.9.2
-
+export LIBXML2_INCLUDE=/usr/include/libxml2
+export LIBXML2_LIBS=/usr/lib/x86_64-linux-gnu
+export BASE=/home/kumar/hcpp
 ################################################
 #
 # DO NOT MODIFY ANYTHING BELOW UNLESS YOU ARE
@@ -11,12 +12,9 @@ export LIBXML2=/usr/local/Cellar/libxml2/2.9.2
 #
 ################################################
 
-cd ..
-BASE=`pwd`
-cd -
 export hcpp=${BASE}
 export HCPP_ROOT=${hcpp}/hcpp-install
-export LD_LIBRARY_PATH=${HCPP_ROOT}/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${HCPP_ROOT}/lib:${LIBXML2_LIBS}:$LD_LIBRARY_PATH
 
 if [ ! -z "${TBB_MALLOC}" ]; then
    export LD_LIBRARY_PATH=${TBB_MALLOC}:$LD_LIBRARY_PATH
