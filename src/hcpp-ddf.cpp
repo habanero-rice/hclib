30d8ff9d46d3980d2b1ec463c4572b196e84bdd4
Vivek Kumar <vivek@vivek-3.local>
2015-11-17 16:49:47 -0600

    fix ddf bug

--- src/hcpp-ddf.cpp
+++ src/hcpp-ddf.cpp
@@ -54,6 +54,7 @@ namespace hcpp {
 
 // For waiting frontier (last element of the list)
 #define UNINITIALIZED_DDF_WAITLIST_PTR ((struct ddt_st *) -1)
+#define EMPTY_DDF_WAITLIST_PTR NULL
 
 /**
  * Associate a DDT to a DDF list.
@@ -69,7 +70,7 @@ void ddt_init(ddt_t * ddt, ddf_t ** ddf_list) {
 ddf_t * ddf_create() {
 	ddf_t * ddf = (ddf_t *) malloc(sizeof(ddf_t));
 	ddf-> kind = DDF_KIND_SHARED;
-	ddf->datum = NULL;
+	ddf->datum = UNINITIALIZED_DDF_DATA_PTR;
 	ddf->headDDTWaitList = UNINITIALIZED_DDF_WAITLIST_PTR;
 	return ddf;
 }
@@ -79,7 +80,7 @@ ddf_t * ddf_create() {
  */
 void ddf_create_preinit(DDF_t* ddf) {
 	ddf-> kind = DDF_KIND_SHARED;
-	ddf->datum = NULL;
+	ddf->datum = UNINITIALIZED_DDF_DATA_PTR;
 	ddf->headDDTWaitList = UNINITIALIZED_DDF_WAITLIST_PTR;
 }
 
@@ -98,6 +99,17 @@ ddf_t ** ddf_create_n(size_t nb_ddfs, int null_terminated) {
 		ddfs[lg] = NULL;
 	}
 	return ddfs;
+}
+
+/**
+ * Get datum from a DDF.
+ * Note: this is concurrent with the 'put' operation.
+ */
+void * ddf_get(ddf_t * ddf) {
+	if (ddf->datum == UNINITIALIZED_DDF_DATA_PTR) {
+		return NULL;
+	}
+	return (void *) ddf->datum;
 }
 
 /**
@@ -123,80 +135,58 @@ void ddf_free(ddf_t * ddf) {
 	free(ddf);
 }
 
-static int register_if_ddf_not_ready(ddt_t * ddt, ddf_t * ddf) {
-	int registered = 0;
-	ddt_t * headDDTWaitList = (ddt_t *) ddf->headDDTWaitList;
-	if (DEBUG_DDF) {
-		printf("ddf: register_if_ddf_not_ready ddt = %p, ddf = %p \n", ddt, ddf);
-	}
-	while (headDDTWaitList != DDF_SATISFIED && !registered) {
-		/* headDDTWaitList can not be DDF_SATISFIED in here */
-		ddt->nextDDTWaitingOnSameDDF = headDDTWaitList;
+__inline__ int __registerIfDDFnotReady_AND ( ddt_t* wrapperTask, DDF_t* ddfToCheck ) {
+    int success = 0;
+    ddt_t* waitListOfDDF = ( ddt_t* ) ddfToCheck->headDDTWaitList;
 
-		// Try to add the ddt to the ddf's list of ddts waiting.
-		registered = __sync_bool_compare_and_swap(&(ddf->headDDTWaitList),
-				headDDTWaitList, ddt);
+    if ( waitListOfDDF != EMPTY_DDF_WAITLIST_PTR ) {
 
-		/* may have failed because either some other task tried to be the head or a put occurred. */
-		if (!registered) {
-			headDDTWaitList = (ddt_t *) ddf->headDDTWaitList;
-			/* if waitListOfDDF was set to DDF_SATISFIED, the loop condition will handle that
-			 * if another task was added, now try to add in front of that
-			 */
-		}
-	}
-	if (DEBUG_DDF && registered) {
-		printf("ddf: %p registered %p as headDDT of ddf %p\n", ddt, ddf->headDDTWaitList, ddf);
-	}
-	return registered;
+        while ( waitListOfDDF != EMPTY_DDF_WAITLIST_PTR && !success ) {
+            /* waitListOfDDF can not be EMPTY_DDF_WAITLIST_PTR in here*/
+            wrapperTask->nextDDTWaitingOnSameDDF = waitListOfDDF;
+
+            success = __sync_bool_compare_and_swap(&(ddfToCheck -> headDDTWaitList), waitListOfDDF, wrapperTask);
+            /* printf("task:%p registered to DDF:%p\n", pollingTask,ddfToCheck); */
+
+            /* may have failed because either some other task tried to be the head or a put occurred. */
+            if ( !success ) {
+                waitListOfDDF = ( ddt_t* ) ddfToCheck -> headDDTWaitList;
+                /* if waitListOfDDF was set to EMPTY_DDF_WAITLIST_PTR, the loop condition will handle that
+                 * if another task was added, now try to add in front of that
+                 * */
+            }
+        }
+
+    }
+
+    return success;
 }
 
 /**
  * Runtime interface to DDTs.
  * Returns '1' if all ddf dependencies have been satisfied.
  */
-int iterate_ddt_frontier(ddt_t * ddt) {
-	ddf_t ** currentDDF = ddt->waitingFrontier;
+int iterate_ddt_frontier ( ddt_t* wrapperTask) {
+	DDF_t** currDDFnodeToWaitOn = wrapperTask->waitingFrontier;
 
-	if (DEBUG_DDF) {
-		printf("ddf: Trying to iterate ddt %p, frontier ddf is %p \n", ddt, currentDDF);
-	}
-	// Try and register on ddf until one is not ready.
-	while ((*currentDDF) && !register_if_ddf_not_ready(ddt, *currentDDF)) {
-		++currentDDF;
-	}
-	// Here we either:
-	// - Have hit a ddf not ready (i.e. no 'put' on this one yet)
-	// - iterated over the whole list (i.e. all 'put' must have happened)
-	if (DEBUG_DDF) { printf("ddf: iterate result %d\n", (*currentDDF == NULL)); }
-	ddt->waitingFrontier = currentDDF;
-	return (*currentDDF == NULL);
+    while (*currDDFnodeToWaitOn && !__registerIfDDFnotReady_AND (wrapperTask, *currDDFnodeToWaitOn) ) {
+        ++currDDFnodeToWaitOn;
+    }
+	wrapperTask->waitingFrontier = currDDFnodeToWaitOn;
+    return *currDDFnodeToWaitOn == NULL;
 }
 
 //
 // Task conversion Implementation
 //
 
-int iterate_ddt_frontier(ddt_t * ddt);
-
 task_t * rt_ddt_to_async_task(ddt_t * ddt) {
-    task_t* t = &(((task_t *)ddt)[-1]);
-    return t;
+	task_t* t = &(((task_t *)ddt)[-1]);
+	return t;
 }
 
 ddt_t * rt_async_task_to_ddt(task_t * async_task) {
-    return &(((hcpp_task_t*) async_task)->ddt);
-}
-
-/**
- * Get datum from a DDF.
- * Note: this is concurrent with the 'put' operation.
- */
-void * ddf_get(ddf_t * ddf) {
-	if (ddf->datum == UNINITIALIZED_DDF_DATA_PTR) {
-		return NULL;
-	}
-	return (void *) ddf->datum;
+	return &(((hcpp_task_t*) async_task)->ddt);
 }
 
 /**
@@ -204,38 +194,36 @@ void * ddf_get(ddf_t * ddf) {
  * Close down registration of DDTs on this DDF and iterate over the
  * DDF's frontier to try to advance DDTs that were waiting on this DDF.
  */
-void ddf_put(ddf_t * ddf, void * datum) {
-	HASSERT(datum != UNINITIALIZED_DDF_DATA_PTR && EMPTY_DATUM_ERROR_MSG);
-	HASSERT(ddf != NULL && "error: can't DDF is a pointer to NULL");
-	//TODO Limitation: not enough to guarantee single assignment
-	if (DEBUG_DDF) { printf("ddf: put datum %p\n", ddf->datum); }
-	HASSERT(ddf->datum == NULL && "error: violated single assignment property for DDFs");
+void ddf_put (ddf_t* ddfToBePut, void * datumToBePut ) {
+	HASSERT (datumToBePut != UNINITIALIZED_DDF_DATA_PTR && EMPTY_DATUM_ERROR_MSG);
+	HASSERT (ddfToBePut != NULL && "can not put into NULL DDF");
+	HASSERT (ddfToBePut-> datum == NULL && "violated single assignment property for DDFs");
 
-	volatile ddt_t * headDDTWaitList = NULL;
-	ddf->datum = datum;
-	headDDTWaitList = ddf->headDDTWaitList;
-	if (DEBUG_DDF) { printf("ddf: Retrieve ddf %p head's ddt %p\n", ddf, headDDTWaitList); }
-	// Try to cas the wait list to prevent new DDTs from registering on this ddf.
-	// When cas successed, new DDTs see the DDF has been satisfied and proceed.
-	while (!__sync_bool_compare_and_swap(&(ddf->headDDTWaitList),
-			(struct ddt_st *) headDDTWaitList, DDF_SATISFIED )) {
-		headDDTWaitList = ddf->headDDTWaitList;
+	volatile ddt_t* waitListOfDDF = NULL;
+	ddt_t* currDDT = NULL;
+	ddt_t* nextDDT = NULL;
+
+	ddfToBePut-> datum = datumToBePut;
+	waitListOfDDF = ddfToBePut->headDDTWaitList;
+	/*seems like I can not avoid a CAS here*/
+	while ( !__sync_bool_compare_and_swap( &(ddfToBePut -> headDDTWaitList), waitListOfDDF, EMPTY_DDF_WAITLIST_PTR)) {
+		waitListOfDDF = ddfToBePut -> headDDTWaitList;
 	}
-	if (DEBUG_DDF) {
-		printf("ddf: Put successful on ddf %p set head's ddf to %p\n", ddf, ddf->headDDTWaitList);
-	}
-	// Now iterate over the list of DDTs, and try to advance their frontier.
-	ddt_t * currDDT = (ddt_t *) headDDTWaitList;
-	while (currDDT != UNINITIALIZED_DDF_WAITLIST_PTR) {
-		if (DEBUG_DDF) { printf("ddf: Trying to iterate frontier of ddt %p\n", currDDT); }
-		if (iterate_ddt_frontier(currDDT)) {
+
+	currDDT = ( ddt_t* ) waitListOfDDF;
+
+	/* printf("DDF:%p was put:%p with value:%d\n", ddfToBePut, datumToBePut,*((int*)datumToBePut)); */
+	while ( currDDT != UNINITIALIZED_DDF_WAITLIST_PTR ) {
+		nextDDT = currDDT->nextDDTWaitingOnSameDDF;
+		if (iterate_ddt_frontier(currDDT) ) {
+			/* printf("pushed:%p\n", currDDT); */
+			/*deque_push_default(currFrame);*/
 			// DDT eligible to scheduling
 			task_t * async_task = rt_ddt_to_async_task(currDDT);
 			if (DEBUG_DDF) { printf("ddf: async_task %p\n", async_task); }
 			try_schedule_async(async_task, 0);
 		}
-		currDDT = currDDT->nextDDTWaitingOnSameDDF;
+		currDDT = nextDDT;
 	}
 }
-
 }
