7640352d5b970f67061109fe23ea6e64cac79570
Max Grossman <jmaxg3@gmail.com>
2016-02-20 10:51:06 -0600

    slightly more verbose argc test

--- test/cpp/access_argc.cpp
+++ test/cpp/access_argc.cpp
@@ -4,7 +4,10 @@
 
 int main(int argc, char **argv) {
     hclib::launch(&argc, argv, [=] {
+        printf("I see argc = %d, argv contains %s\n", argc, argv[0]);
         assert(argc == 1);
+        assert(strcmp(argv[0], "./access_argc") == 0);
+        printf("Check results: OK\n");
     });
     return 0;
 }
