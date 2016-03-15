ba3885b35ec5c7d3fd31cda6200e0afe3d0ac68a
Vivek Kumar <vk.aries@gmail.com>
2016-03-15 12:12:01 -0500

    setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-export TBB_MALLOC=/home/kumar/tbb
-#export HCLIB_FLAGS="--enable-production"
+# Please set the following 4 environment variables
+
+# Absolute path to hcpp directory
+export HCLIB_BASE=/home/kumar/openshmem/hcpp
+
+# Absolute path to installation directory of OpenSHMEM
+export OPENSHMEM_ROOT=/home/kumar/openshmem/install-openshmem
+
+# Path to libxml2 headers
 export LIBXML2_INCLUDE=/usr/include/libxml2
+
+# Path to libxml2 libraries
 export LIBXML2_LIBS=/usr/lib/x86_64-linux-gnu
-export BASE=/home/kumar/hcpp
+
 ################################################
 #
 # DO NOT MODIFY ANYTHING BELOW UNLESS YOU ARE
-# CHANGING THE INSTALLATION PATH OF HCLIB
+# CHANGING THE INSTALLATION PATH OF HABANERO-UPC
 #
 ################################################
 
-export hclib=${BASE}
-export HCLIB_ROOT=${hclib}/hclib-install
-export LD_LIBRARY_PATH=${HCLIB_ROOT}/lib:${LIBXML2_LIBS}:$LD_LIBRARY_PATH
-
-if [ ! -z "${TBB_MALLOC}" ]; then
-   export LD_LIBRARY_PATH=${TBB_MALLOC}:$LD_LIBRARY_PATH
-fi
+export SHMEM_CXX=oshcxx
+export HCLIB_ROOT=${HCLIB_BASE}/hclib-install
+export PATH=${OPENSHMEM_ROOT}/bin:$PATH
+export LD_LIBRARY_PATH=${HCLIB_ROOT}/lib:${LIBXML2_LIBS}:${OPENSHMEM_ROOT}/lib:$LD_LIBRARY_PATH
