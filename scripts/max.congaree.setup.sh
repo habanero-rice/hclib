b52843bc2d5f5cc68c4b92091c125a2319468d38
Max Grossman <jmaxg3@gmail.com>
2016-03-23 19:58:27 -0500

    disable cuda by default on congaree

--- scripts/max.congaree.setup.sh
+++ scripts/max.congaree.setup.sh
@@ -5,7 +5,7 @@ export TBB_MALLOC=/home/kumar/tbb
 export LIBXML2_INCLUDE=/usr/include/libxml2
 export LIBXML2_LIBS=/usr/lib/x86_64-linux-gnu
 export BASE=/home/mg1/hcpp
-export HC_CUDA_FLAGS=--enable-cuda
+# export HC_CUDA_FLAGS=--enable-cuda
 
 ################################################
 #
