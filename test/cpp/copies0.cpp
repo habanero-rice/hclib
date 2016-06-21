d30032badf1c0529bbc6c84a1e00a05a3f5a352d
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2016-06-21 13:48:01 -0500

    Add tests for number of times lambdas are copied

---
+++ test/cpp/copies0.cpp
@@ -0,0 +1,70 @@
+/* Copyright (c) 2013, Rice University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+1.  Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+2.  Redistributions in binary form must reproduce the above
+     copyright notice, this list of conditions and the following
+     disclaimer in the documentation and/or other materials provided
+     with the distribution.
+3.  Neither the name of Rice University
+     nor the names of its contributors may be used to endorse or
+     promote products derived from this software without specific
+     prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/**
+ * DESC: Counting lambda copies for a simple async
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "hclib_cpp.h"
+
+struct CopyCounter {
+    int copies;
+    int moves;
+
+    CopyCounter(): copies(0), moves(0) {
+        printf("Created new counter object.\n");
+    }
+
+    CopyCounter(const CopyCounter &other):
+        copies(other.copies+1), moves(other.moves) { }
+
+    CopyCounter(const CopyCounter &&other):
+        copies(other.copies), moves(other.moves+1) { }
+};
+
+int main (int argc, char ** argv) {
+    hclib::launch([]() {
+        hclib::finish([]() {
+            CopyCounter k {};
+            hclib::async([k] {
+                printf("Counted %d copies, %d moves...\n", k.copies, k.moves);
+                // Copy 1: capturing "k" by-value into the lambda
+                // Copy 2: copying the lambda (and its closure) into the heap
+                //assert(k.copies + k.moves <= 2);
+            });
+        });
+    });
+    printf("Exiting...\n");
+    return 0;
+}
