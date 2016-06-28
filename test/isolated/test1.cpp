eed8af1bdd3755d9545a24cce53f6e6dbc81d7c1
Vivek Kumar <vivek@staff-84-dun20-089.rice.edu>
2016-06-28 15:42:04 -0500

    update testcase for isolation

--- test/isolated/test1.cpp
+++ test/isolated/test1.cpp
@@ -1,18 +1,28 @@
 #include "hclib_cpp.h"
 
 using namespace std;
+#define LIMIT 1000
 
 int main (int argc, char ** argv) {
   hclib::launch([&]() {
-    double *ptr = new double;
+    int *ptr = new int;
+    *ptr = 0;
 
     hclib::enable_isolation(ptr);
 
-    hclib::isolated(ptr, [=]() {
-      *ptr = 100.9;
-      printf("Isolated execution\n");
+    hclib::finish([=]() {
+      for(int i=0; i<LIMIT; i++) {
+        hclib::async([=]() {
+          hclib::isolated(ptr, [=]() {
+            *ptr += 1;
+          });
+        });
+      }
     });
 
+    printf("Result=%d\n",*ptr);
+    assert(*ptr==LIMIT && "Test Failed");
+    printf("Test Passed\n");
     hclib::disable_isolation(ptr);
   });
   return 0;
