08aaf293449a457d38997c70b7ec544df6cf2307
Max Grossman <jmaxg3@gmail.com>
2017-02-13 15:45:38 -0600

    add test for reduction pattern

---
+++ test/performance-regression/micro/reduce_var.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define NREDUCERS 10000
---
+++ test/performance-regression/micro/reduce_var_omp.c
@@ -0,0 +1,39 @@
+#include "hclib.h"
+
+#include <omp.h>
+#include <stdio.h>
+#include "reduce_var.h"
+
+int main(int argc, char **argv) {
+    int i;
+
+    int nthreads;
+#pragma omp parallel
+#pragma omp master
+    {
+        nthreads = omp_get_num_threads();
+    }
+    printf("Using %d OpenMP threads\n", nthreads);
+
+#pragma omp parallel
+#pragma omp master
+    {
+        int i;
+
+        unsigned sum = 0;
+        const unsigned long long start_time = hclib_current_time_ns();
+#pragma omp taskloop private(i) reduction(+:sum)
+        for (i = 0; i < NREDUCERS; i++) {
+            sum += 1;
+        }
+        const unsigned long long end_time = hclib_current_time_ns();
+
+        assert(sum == NREDUCERS);
+
+        printf("OpenMP reductions at a rate of %f reducers/ms\n",
+                (double)NREDUCERS / ((double)(end_time -
+                        start_time) / 1000.0));
+    }
+
+    return 0;
+}
