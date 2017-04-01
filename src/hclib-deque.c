DaoWen/hclib-debug

Use C11 stdatomic.h for internal atomic ops

src/hclib-deque.c
@@ -39,30 +39,36 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "hclib-internal.h"
 #include "hclib-atomics.h"
 
-void deque_init(deque_t *deq, void *init_value) {
+#if 0 // UNUSED
+void deque_init(deque_t *deq) {
     deq->head = 0;
+    //@ FIXME ATOMIC: this should be a RELEASE to ensure synchronization?
     deq->tail = 0;
 }
+
+void deque_destroy(deque_t *deq) {
+    free(deq);
+}
+#endif
 
 /*
  * push an entry onto the tail of the deque
  */
-int deque_push(deque_t *deq, void *entry) {
-    int size = deq->tail - deq->head;
+int deque_push(deque_t *deq, hclib_task_t *entry) {
+    int tail = _hclib_atomic_load_relaxed(&deq->tail);
+    int head = _hclib_atomic_load_relaxed(&deq->head);
+    int size = tail - head;
     if (size == INIT_DEQUE_CAPACITY) { /* deque looks full */
         /* may not grow the deque if some interleaving steal occur */
         // std::cout<<getenv("PMI_RANK") <<": Deque full for worker-"<<current_ws()->id << std::endl;
         // HASSERT("DEQUE full, increase deque's size " && 0);
         return 0;
     }
-    int n = (deq->tail) % INIT_DEQUE_CAPACITY;
-    deq->data[n] = (hclib_task_t *) entry;
-    deq->tail++;
+    int n = tail % INIT_DEQUE_CAPACITY;
+    deq->data[n] = entry;
+    //@ ATOMIC: this should be a RELEASE to ensure synchronization
+    _hclib_atomic_inc_release(&deq->tail);
     return 1;
-}
-
-void deque_destroy(deque_t *deq) {
-    free(deq);
 }
 
 /*
@@ -78,16 +84,18 @@ hclib_task_t *deque_steal(deque_t *deq) {
      */
     int tail;
 
-    head = deq->head;
-    hc_mfence();
-    tail = deq->tail;
+    head = _hclib_atomic_load_relaxed(&deq->head);
+    // ATOMIC: load acquire
+    // We want all the writes from the producing thread to read the task data
+    // and we're using the tail as the synchronization variable
+    tail = _hclib_atomic_load_acquire(&deq->tail);
     if ((tail - head) <= 0) {
         return NULL;
     }
 
-    hclib_task_t *t = (hclib_task_t *) deq->data[head % INIT_DEQUE_CAPACITY];
+    hclib_task_t *t = deq->data[head % INIT_DEQUE_CAPACITY];
     /* compete with other thieves and possibly the owner (if the size == 1) */
-    if (hc_cas(&deq->head, head, head + 1)) { /* competing */
+    if (_hclib_atomic_cas_relaxed(&deq->head, head, head + 1)) { /* competing */
         return t;
     }
     return NULL;
@@ -97,31 +105,29 @@ hclib_task_t *deque_steal(deque_t *deq) {
  * pop the task out of the deque from the tail
  */
 hclib_task_t *deque_pop(deque_t *deq) {
-    hc_mfence();
-    int tail = deq->tail;
-    tail--;
-    deq->tail = tail;
-    hc_mfence();
-    int head = deq->head;
+    int tail = _hclib_atomic_dec_relaxed(&deq->tail);
+    int head = _hclib_atomic_load_relaxed(&deq->head);
 
     int size = tail - head;
     if (size < 0) {
-        deq->tail = deq->head;
+        _hclib_atomic_store_relaxed(&deq->tail, head);
         return NULL;
     }
-    hclib_task_t *t = (hclib_task_t *) deq->data[(tail) % INIT_DEQUE_CAPACITY];
+    hclib_task_t *t = deq->data[tail % INIT_DEQUE_CAPACITY];
 
     if (size > 0) {
         return t;
     }
 
-    /* now size == 1, I need to compete with the thieves */
-    if (!hc_cas(&deq->head, head, head + 1)) {
+    /* now the deque appears empty */
+    /* I need to compete with the thieves for the last task */
+    //@- if (!hc_cas(&deq->head, head, head + 1)) {
+    if (!_hclib_atomic_cas_relaxed(&deq->head, head, head + 1)) {
         t = NULL;
     }
 
-    /* now the deque is empty */
-    deq->tail = deq->head;
+    _hclib_atomic_inc_relaxed(&deq->tail);
+
     return t;
 }
 
@@ -129,7 +135,8 @@ hclib_task_t *deque_pop(deque_t *deq) {
 /* Semi Concurrent DEQUE                              */
 /******************************************************/
 
-void semi_conc_deque_init(semi_conc_deque_t *semiDeq, void *initValue) {
+#if 0 // UNUSED
+void semi_conc_deque_init(semi_conc_deque_t *semiDeq) {
     deque_t *deq = &semiDeq->deque;
     deq->head = 0;
     deq->tail = 0;
@@ -140,7 +147,7 @@ void semi_conc_deque_destroy(semi_conc_deque_t *semiDeq) {
     free(semiDeq);
 }
 
-void semi_conc_deque_locked_push(semi_conc_deque_t *semiDeq, void *entry) {
+void semi_conc_deque_locked_push(semi_conc_deque_t *semiDeq, hclib_task_t *entry) {
     deque_t *deq = &semiDeq->deque;
     int success = 0;
     while (!success) {
@@ -172,5 +179,6 @@ hclib_task_t *semi_conc_deque_non_locked_pop(semi_conc_deque_t *semiDeq) {
     }
     return NULL;
 }
+#endif
 
 

src/inc/hclib-atomics.h
@@ -31,7 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /*
  * hclib-atomics.h
- *  
+ *
  *      Author: Vivek Kumar (vivekk@rice.edu)
  *      Acknowledgments: https://wiki.rice.edu/confluence/display/HABANERO/People
  */
@@ -39,58 +39,120 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef HCLIB_ATOMICS_H_
 #define HCLIB_ATOMICS_H_
 
-#if defined __x86_64 || __i686__
+#include <stdbool.h>
 
-#define HC_CACHE_LINE 64
+#ifdef HAVE_C11_STDATOMIC
 
+#include <stdatomic.h>
 
-static __inline__ int hc_atomic_inc(volatile int *ptr) {
-    unsigned char c;
-    __asm__ __volatile__(
-
-            "lock       ;\n"
-            "incl %0; sete %1"
-            : "+m" (*(ptr)), "=qm" (c)
-              : : "memory"
-    );
-    return c != 0;
+static inline int _hclib_atomic_load_relaxed(_Atomic int *target) {
+    return atomic_load_explicit(target, memory_order_relaxed);
 }
 
-/*
- * return 1 if the *ptr becomes 0 after decremented, otherwise return 0
- */
-static __inline__ int hc_atomic_dec(volatile int *ptr) {
-    unsigned char rt;
-    __asm__ __volatile__(
-            "lock;\n"
-            "decl %0; sete %1"
-            : "+m" (*(ptr)), "=qm" (rt)
-              : : "memory"
-    );
-    return rt != 0;
+static inline int _hclib_atomic_load_acquire(_Atomic int *target) {
+    return atomic_load_explicit(target, memory_order_acquire);
 }
 
-static __inline__ void hc_mfence() {
-        __asm__ __volatile__("mfence":: : "memory");
+static inline void _hclib_atomic_store_relaxed(_Atomic int *target, int value) {
+    atomic_store_explicit(target, value, memory_order_relaxed);
 }
 
-/*
- * if (*ptr == ag) { *ptr = x, return 1 }
- * else return 0;
- */
-static __inline__ int hc_cas(volatile int *ptr, int ag, int x) {
-        int tmp;
-        __asm__ __volatile__("lock;\n"
-                             "cmpxchgl %1,%3"
-                             : "=a" (tmp) /* %0 EAX, return value */
-                             : "r"(x), /* %1 reg, new value */
-                               "0" (ag), /* %2 EAX, compare value */
-                               "m" (*(ptr)) /* %3 mem, destination operand */
-                             : "memory" /*, "cc" content changed, memory and cond register */
-                );
-        return tmp == ag;
+static inline void _hclib_atomic_store_release(_Atomic int *target, int value) {
+    atomic_store_explicit(target, value, memory_order_release);
 }
 
-#endif /* __x86_64 */
+static inline int _hclib_atomic_inc_relaxed(_Atomic int *target) {
+    return atomic_fetch_add_explicit(target, 1, memory_order_relaxed) + 1;
+}
+
+static inline int _hclib_atomic_inc_acquire(_Atomic int *target) {
+    return atomic_fetch_add_explicit(target, 1, memory_order_acquire) + 1;
+}
+
+static inline int _hclib_atomic_inc_release(_Atomic int *target) {
+    return atomic_fetch_add_explicit(target, 1, memory_order_release) + 1;
+}
+
+static inline int _hclib_atomic_inc_acq_rel(_Atomic int *target) {
+    return atomic_fetch_add_explicit(target, 1, memory_order_acq_rel) + 1;
+}
+
+static inline int _hclib_atomic_dec_relaxed(_Atomic int *target) {
+    return atomic_fetch_sub_explicit(target, 1, memory_order_relaxed) - 1;
+}
+
+static inline int _hclib_atomic_dec_release(_Atomic int *target) {
+    return atomic_fetch_sub_explicit(target, 1, memory_order_release) - 1;
+}
+
+static inline int _hclib_atomic_dec_acq_rel(_Atomic int *target) {
+    return atomic_fetch_sub_explicit(target, 1, memory_order_acq_rel) - 1;
+}
+
+static inline bool _hclib_atomic_cas_relaxed(_Atomic int *target, int expected, int desired) {
+    return atomic_compare_exchange_strong_explicit(target, &expected, desired,
+            memory_order_relaxed, memory_order_relaxed);
+}
+
+#else /* !HAVE_C11_STDATOMIC */
+
+#warning "Missing C11 atomics support, falling back to gcc atomics."
+
+#ifndef _Atomic
+#define _Atomic volatile
+#endif
+
+static inline int _hclib_atomic_load_relaxed(_Atomic int *target) {
+    return *target;
+}
+
+static inline int _hclib_atomic_load_acquire(_Atomic int *target) {
+    int res = *target;
+    __sync_synchronize(); // acquire after read
+    return res;
+}
+
+static inline void _hclib_atomic_store_relaxed(_Atomic int *target, int value) {
+    *target = value;
+}
+
+static inline void _hclib_atomic_store_release(_Atomic int *target, int value) {
+    __sync_synchronize(); // release before write
+    *target = value;
+}
+
+static inline int _hclib_atomic_inc_relaxed(_Atomic int *target) {
+    return __sync_add_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_inc_acquire(_Atomic int *target) {
+    return __sync_add_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_inc_release(_Atomic int *target) {
+    return __sync_add_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_inc_acq_rel(_Atomic int *target) {
+    return __sync_add_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_dec_relaxed(_Atomic int *target) {
+    return __sync_sub_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_dec_release(_Atomic int *target) {
+    return __sync_sub_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_dec_acq_rel(_Atomic int *target) {
+    return __sync_sub_and_fetch(target, 1);
+}
+
+static inline bool _hclib_atomic_cas_relaxed(_Atomic int *target, int expected, int desired) {
+    return __sync_bool_compare_and_swap(target, expected, desired);
+}
+
+#endif /* HAVE_C11_STDATOMIC */
 
 #endif /* HCLIB_ATOMICS_H_ */

habanero-rice/hclib

src/hclib-deque.c
@@ -39,30 +39,36 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "hclib-internal.h"
 #include "hclib-atomics.h"
 
-void deque_init(deque_t *deq, void *init_value) {
+#if 0 // UNUSED
+void deque_init(deque_t *deq) {
     deq->head = 0;
+    //@ FIXME ATOMIC: this should be a RELEASE to ensure synchronization?
     deq->tail = 0;
 }
+
+void deque_destroy(deque_t *deq) {
+    free(deq);
+}
+#endif
 
 /*
  * push an entry onto the tail of the deque
  */
-int deque_push(deque_t *deq, void *entry) {
-    int size = deq->tail - deq->head;
+int deque_push(deque_t *deq, hclib_task_t *entry) {
+    int tail = _hclib_atomic_load_relaxed(&deq->tail);
+    int head = _hclib_atomic_load_relaxed(&deq->head);
+    int size = tail - head;
     if (size == INIT_DEQUE_CAPACITY) { /* deque looks full */
         /* may not grow the deque if some interleaving steal occur */
         // std::cout<<getenv("PMI_RANK") <<": Deque full for worker-"<<current_ws()->id << std::endl;
         // HASSERT("DEQUE full, increase deque's size " && 0);
         return 0;
     }
-    int n = (deq->tail) % INIT_DEQUE_CAPACITY;
-    deq->data[n] = (hclib_task_t *) entry;
-    deq->tail++;
+    int n = tail % INIT_DEQUE_CAPACITY;
+    deq->data[n] = entry;
+    //@ ATOMIC: this should be a RELEASE to ensure synchronization
+    _hclib_atomic_inc_release(&deq->tail);
     return 1;
-}
-
-void deque_destroy(deque_t *deq) {
-    free(deq);
 }
 
 /*
@@ -78,16 +84,18 @@ hclib_task_t *deque_steal(deque_t *deq) {
      */
     int tail;
 
-    head = deq->head;
-    hc_mfence();
-    tail = deq->tail;
+    head = _hclib_atomic_load_relaxed(&deq->head);
+    // ATOMIC: load acquire
+    // We want all the writes from the producing thread to read the task data
+    // and we're using the tail as the synchronization variable
+    tail = _hclib_atomic_load_acquire(&deq->tail);
     if ((tail - head) <= 0) {
         return NULL;
     }
 
-    hclib_task_t *t = (hclib_task_t *) deq->data[head % INIT_DEQUE_CAPACITY];
+    hclib_task_t *t = deq->data[head % INIT_DEQUE_CAPACITY];
     /* compete with other thieves and possibly the owner (if the size == 1) */
-    if (hc_cas(&deq->head, head, head + 1)) { /* competing */
+    if (_hclib_atomic_cas_acq_rel(&deq->head, head, head + 1)) { /* competing */
         return t;
     }
     return NULL;
@@ -97,31 +105,29 @@ hclib_task_t *deque_steal(deque_t *deq) {
  * pop the task out of the deque from the tail
  */
 hclib_task_t *deque_pop(deque_t *deq) {
-    hc_mfence();
-    int tail = deq->tail;
-    tail--;
-    deq->tail = tail;
-    hc_mfence();
-    int head = deq->head;
+    int tail = _hclib_atomic_dec_relaxed(&deq->tail);
+    int head = _hclib_atomic_load_relaxed(&deq->head);
 
     int size = tail - head;
     if (size < 0) {
-        deq->tail = deq->head;
+        _hclib_atomic_store_relaxed(&deq->tail, head);
         return NULL;
     }
-    hclib_task_t *t = (hclib_task_t *) deq->data[(tail) % INIT_DEQUE_CAPACITY];
+    hclib_task_t *t = deq->data[tail % INIT_DEQUE_CAPACITY];
 
     if (size > 0) {
         return t;
     }
 
-    /* now size == 1, I need to compete with the thieves */
-    if (!hc_cas(&deq->head, head, head + 1)) {
+    /* now the deque appears empty */
+    /* I need to compete with the thieves for the last task */
+    //@- if (!hc_cas(&deq->head, head, head + 1)) {
+    if (!_hclib_atomic_cas_acq_rel(&deq->head, head, head + 1)) {
         t = NULL;
     }
 
-    /* now the deque is empty */
-    deq->tail = deq->head;
+    _hclib_atomic_inc_relaxed(&deq->tail);
+
     return t;
 }
 
@@ -129,7 +135,8 @@ hclib_task_t *deque_pop(deque_t *deq) {
 /* Semi Concurrent DEQUE                              */
 /******************************************************/
 
-void semi_conc_deque_init(semi_conc_deque_t *semiDeq, void *initValue) {
+#if 0 // UNUSED
+void semi_conc_deque_init(semi_conc_deque_t *semiDeq) {
     deque_t *deq = &semiDeq->deque;
     deq->head = 0;
     deq->tail = 0;
@@ -140,7 +147,7 @@ void semi_conc_deque_destroy(semi_conc_deque_t *semiDeq) {
     free(semiDeq);
 }
 
-void semi_conc_deque_locked_push(semi_conc_deque_t *semiDeq, void *entry) {
+void semi_conc_deque_locked_push(semi_conc_deque_t *semiDeq, hclib_task_t *entry) {
     deque_t *deq = &semiDeq->deque;
     int success = 0;
     while (!success) {
@@ -172,5 +179,6 @@ hclib_task_t *semi_conc_deque_non_locked_pop(semi_conc_deque_t *semiDeq) {
     }
     return NULL;
 }
+#endif
 
 

src/inc/hclib-atomics.h
@@ -31,7 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /*
  * hclib-atomics.h
- *  
+ *
  *      Author: Vivek Kumar (vivekk@rice.edu)
  *      Acknowledgments: https://wiki.rice.edu/confluence/display/HABANERO/People
  */
@@ -39,58 +39,122 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef HCLIB_ATOMICS_H_
 #define HCLIB_ATOMICS_H_
 
-#if defined __x86_64 || __i686__
+#include <stdbool.h>
 
-#define HC_CACHE_LINE 64
+#ifdef HAVE_C11_STDATOMIC
 
+#include <stdatomic.h>
 
-static __inline__ int hc_atomic_inc(volatile int *ptr) {
-    unsigned char c;
-    __asm__ __volatile__(
-
-            "lock       ;\n"
-            "incl %0; sete %1"
-            : "+m" (*(ptr)), "=qm" (c)
-              : : "memory"
-    );
-    return c != 0;
+static inline int _hclib_atomic_load_relaxed(_Atomic int *target) {
+    return atomic_load_explicit(target, memory_order_relaxed);
 }
 
-/*
- * return 1 if the *ptr becomes 0 after decremented, otherwise return 0
- */
-static __inline__ int hc_atomic_dec(volatile int *ptr) {
-    unsigned char rt;
-    __asm__ __volatile__(
-            "lock;\n"
-            "decl %0; sete %1"
-            : "+m" (*(ptr)), "=qm" (rt)
-              : : "memory"
-    );
-    return rt != 0;
+static inline int _hclib_atomic_load_acquire(_Atomic int *target) {
+    return atomic_load_explicit(target, memory_order_acquire);
 }
 
-static __inline__ void hc_mfence() {
-        __asm__ __volatile__("mfence":: : "memory");
+static inline void _hclib_atomic_store_relaxed(_Atomic int *target, int value) {
+    atomic_store_explicit(target, value, memory_order_relaxed);
 }
 
-/*
- * if (*ptr == ag) { *ptr = x, return 1 }
- * else return 0;
- */
-static __inline__ int hc_cas(volatile int *ptr, int ag, int x) {
-        int tmp;
-        __asm__ __volatile__("lock;\n"
-                             "cmpxchgl %1,%3"
-                             : "=a" (tmp) /* %0 EAX, return value */
-                             : "r"(x), /* %1 reg, new value */
-                               "0" (ag), /* %2 EAX, compare value */
-                               "m" (*(ptr)) /* %3 mem, destination operand */
-                             : "memory" /*, "cc" content changed, memory and cond register */
-                );
-        return tmp == ag;
+static inline void _hclib_atomic_store_release(_Atomic int *target, int value) {
+    atomic_store_explicit(target, value, memory_order_release);
 }
 
-#endif /* __x86_64 */
+static inline int _hclib_atomic_inc_relaxed(_Atomic int *target) {
+    return atomic_fetch_add_explicit(target, 1, memory_order_relaxed) + 1;
+}
+
+static inline int _hclib_atomic_inc_acquire(_Atomic int *target) {
+    return atomic_fetch_add_explicit(target, 1, memory_order_acquire) + 1;
+}
+
+static inline int _hclib_atomic_inc_release(_Atomic int *target) {
+    return atomic_fetch_add_explicit(target, 1, memory_order_release) + 1;
+}
+
+static inline int _hclib_atomic_inc_acq_rel(_Atomic int *target) {
+    return atomic_fetch_add_explicit(target, 1, memory_order_acq_rel) + 1;
+}
+
+static inline int _hclib_atomic_dec_relaxed(_Atomic int *target) {
+    return atomic_fetch_sub_explicit(target, 1, memory_order_relaxed) - 1;
+}
+
+static inline int _hclib_atomic_dec_release(_Atomic int *target) {
+    return atomic_fetch_sub_explicit(target, 1, memory_order_release) - 1;
+}
+
+static inline int _hclib_atomic_dec_acq_rel(_Atomic int *target) {
+    return atomic_fetch_sub_explicit(target, 1, memory_order_acq_rel) - 1;
+}
+
+static inline bool _hclib_atomic_cas_acq_rel(_Atomic int *target, int expected, int desired) {
+    return atomic_compare_exchange_strong_explicit(target, &expected, desired,
+            memory_order_acq_rel, memory_order_relaxed);
+}
+
+#else /* !HAVE_C11_STDATOMIC */
+
+#warning "Missing C11 atomics support, falling back to gcc atomics."
+
+#ifndef _Atomic
+#define _Atomic volatile
+#endif
+
+static inline int _hclib_atomic_load_relaxed(_Atomic int *target) {
+    return *target;
+}
+
+static inline int _hclib_atomic_load_acquire(_Atomic int *target) {
+    int res = *target;
+    __sync_synchronize(); // acquire after read
+    return res;
+}
+
+static inline void _hclib_atomic_store_relaxed(_Atomic int *target, int value) {
+    *target = value;
+}
+
+static inline void _hclib_atomic_store_release(_Atomic int *target, int value) {
+    __sync_synchronize(); // release before write
+    *target = value;
+}
+
+static inline int _hclib_atomic_inc_relaxed(_Atomic int *target) {
+    return __sync_add_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_inc_acquire(_Atomic int *target) {
+    return __sync_add_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_inc_release(_Atomic int *target) {
+    return __sync_add_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_inc_acq_rel(_Atomic int *target) {
+    return __sync_add_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_dec_relaxed(_Atomic int *target) {
+    return __sync_sub_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_dec_release(_Atomic int *target) {
+    return __sync_sub_and_fetch(target, 1);
+}
+
+static inline int _hclib_atomic_dec_acq_rel(_Atomic int *target) {
+    return __sync_sub_and_fetch(target, 1);
+}
+
+static inline bool _hclib_atomic_cas_acq_rel(_Atomic int *target, int expected, int desired) {
+    // NOTE - Clang 3.5 has a bug with __sync_bool_compare_and_swap:
+    // https://bugs.llvm.org//show_bug.cgi?format=multiple&id=21499
+    return __sync_val_compare_and_swap(target, expected, desired) == expected;
+}
+
+#endif /* HAVE_C11_STDATOMIC */
 
 #endif /* HCLIB_ATOMICS_H_ */