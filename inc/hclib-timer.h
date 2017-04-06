3fec36dd0baee1ed66b27a94da891c43693b6c49
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-04-05 23:49:45 -0500

    Update license info for base source code and scripts

--- inc/hclib-timer.h
+++ inc/hclib-timer.h
@@ -1,12 +1,17 @@
 /*
- * hclib-timer.h
- *  
- *      Author: Vivek Kumar (vivekk@rice.edu)
- */
-
-/*
- * Variant of the implementation of ss_setState() from the open source
- * implementation of UTS benchmark: http://sourceforge.net/projects/uts-benchmark/files/
+ * Copyright 2017 Rice University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef HCLIB_TIMER_H_
--- src/hclib-timer.c
+++ src/hclib-timer.c
@@ -1,12 +1,25 @@
 /*
- * hclib-timer.cpp
+ * Copyright 2017 Rice University
  *
- *      Author: Vivek Kumar (vivekk@rice.edu)
- */
-
-/*
- * Variant of the implementation of hclib_setState() from the open source
- * implementation of UTS benchmark: http://sourceforge.net/projects/uts-benchmark/files/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * The source code in this file is based on similar code
+ * provided as part of the UTS (Unbalanced Tree Search) benchmark:
+ *
+ * https://sourceforge.net/p/uts-benchmark/code/ci/722e89/tree/stats.c
+ *
+ * Please see the accompanying NOTICE file for license details.
  */
 
 #include <stdio.h>
