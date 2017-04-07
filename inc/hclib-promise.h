6ad778ac6dbf6bad6181e30fe58108fcf3ca417b
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-04-07 15:12:50 -0500

    Switch license to Apache 2.0
    Advantages over our current (BSD 3-clause) license include:
     - Explicit patent clause
     - Explicit contribution clause, which makes handling 3rd-party
       contributions more straightforward
     - Requirement for NOTICE text in derivative works, which makes
       including 3rd-party code in our project less cumbersome

--- inc/hclib-promise.h
+++ inc/hclib-promise.h
@@ -1,37 +1,29 @@
-/* Copyright (c) 2015, Rice University
+/*
+ * Copyright 2017 Rice University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
+#ifndef HCLIB_PROMISE_H_
+#define HCLIB_PROMISE_H_
 
-1.  Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-2.  Redistributions in binary form must reproduce the above
-     copyright notice, this list of conditions and the following
-     disclaimer in the documentation and/or other materials provided
-     with the distribution.
-3.  Neither the name of Rice University
-     nor the names of its contributors may be used to endorse or
-     promote products derived from this software without specific
-     prior written permission.
+#include <stdlib.h>
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+/**
+ * @file User Interface to HCLIB's futures and promises.
  */
 
 /*
- * hclib-promise.h
- *
  * NOTE: Terminology
  *   promise = an object that has data put on it, and by being put on can
  *             trigger downstream dependent tasks to execute. Promises are
@@ -42,19 +34,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *   triggered task = a normal task whose execution is predicated on the
  *             satisfaction of some promise. This dependency is expressed using
  *             the future associated with that promise.
- *  
- *      Author: Vivek Kumar (vivekk@rice.edu), Max Grossman (jmg3@rice.edu)
- *      Ported from hclib
- *      Acknowledgments: https://wiki.rice.edu/confluence/display/HABANERO/People
- */
-
-#ifndef HCLIB_PROMISE_H_
-#define HCLIB_PROMISE_H_
-
-#include <stdlib.h>
-
-/**
- * @file User Interface to HCLIB's futures and promises.
  */
 
 /**
