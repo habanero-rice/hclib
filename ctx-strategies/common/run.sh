d9b01dde5d4e64cdb970c9e2f9b377c023641123
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-06-23 15:28:15 -0500

    Strategy tests: use help-first with hcc

--- ctx-strategies/common/run.sh
+++ ctx-strategies/common/run.sh
@@ -3,6 +3,7 @@
 PROJECT_NAME=$(basename $PWD)
 
 do_test() {
+    set -x
     eval make $1 $PROJECT_MAKE_ARGS
     eval ./$1 $HC_BIN_FLAGS $PROJECT_RUN_ARGS
 }
@@ -10,4 +11,6 @@ do_test() {
 export HCLIB_WORKERS=4
 do_test $PROJECT_NAME
 do_test nb_$PROJECT_NAME
-HC_BIN_FLAGS="-nproc $HCLIB_WORKERS" do_test hclang_$PROJECT_NAME
+# --wf -> work first
+# --hf -> help first
+HC_BIN_FLAGS="--nproc $HCLIB_WORKERS --hf" do_test hclang_$PROJECT_NAME
