f98ca652bed5f2e301562e8f37fe5afa73a04aa5
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-07-09 19:50:37 -0500

    Strategy tests: use --no-join in all builds
    This should lessen the overhead for the final hclib::launch sync for
    benchmarks that might use that for synchronization. It also helps avoid
    segfaults when running the benchmarks on OS X.

--- multi-install.sh
+++ multi-install.sh
@@ -6,19 +6,19 @@ rm -rf compileTree hclib-install
 
 BASE_PREFIX=$PWD/hclib-install/multi
 
-env INSTALL_PREFIX=$PWD/hclib-install ./install.sh
+env INSTALL_PREFIX=$PWD/hclib-install $(./scripts/hclib-options --no-join --fibers --help-finish) ./install.sh
 rm -rf compileTree
 
-env INSTALL_PREFIX=$BASE_PREFIX/fibers $(./scripts/hclib-options --fibers) ./install.sh
+env INSTALL_PREFIX=$BASE_PREFIX/fibers $(./scripts/hclib-options --no-join --fibers) ./install.sh
 rm -rf compileTree
 
-env INSTALL_PREFIX=$BASE_PREFIX/non-blocking $(./scripts/hclib-options --fixed --help-global) ./install.sh
+env INSTALL_PREFIX=$BASE_PREFIX/non-blocking $(./scripts/hclib-options --no-join --fixed --help-global) ./install.sh
 rm -rf compileTree
 
-env INSTALL_PREFIX=$BASE_PREFIX/threads $(./scripts/hclib-options --threads) ./install.sh
+env INSTALL_PREFIX=$BASE_PREFIX/threads $(./scripts/hclib-options --no-join --threads) ./install.sh
 rm -rf compileTree
 
-env INSTALL_PREFIX=$BASE_PREFIX/threads-help $(./scripts/hclib-options --threads --help-finish) ./install.sh
+env INSTALL_PREFIX=$BASE_PREFIX/threads-help $(./scripts/hclib-options --no-join --threads --help-finish) ./install.sh
 rm -rf compileTree
 
 # default is fibers + help-finish
